6712f51ad9897d5ce2685a09006cb9b156a67992
abdo <abdo.e.mosa@gmail.com>
2024-10-29 14:23:10 +0300

    Add splash and welcome screens with routing functionality
    - Create `AppRouting` class to manage navigation between splash and welcome screens
    - Implement `SplashScreen` and `WelcomeScreen` widgets
    - Add `SplashScreenBody` with a timer for transition to the welcome screen
    - Update `main.dart` to initialize routes and set the splash screen as the initial route
    - Include splash screen image in assets folder

---
+++ lib/core/routes/app_routing.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerce_ui_fruits_app/core/routes/routes.dart';
+import 'package:flutter/material.dart';
+
+import '../../features/splash/screen/ui/splash_screen.dart';
+import '../../features/welcome/screen/ui/welcome_screen.dart';
+
+class AppRouting {
+  Route? generateRoutes(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashScreen:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case Routes.welcomeScreen:
+        return MaterialPageRoute(builder: (_) => WelcomeScreen());
+    }
+  }
+}
---
+++ lib/features/splash/screen/widgets/splash_screen_body.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../../../core/routes/routes.dart';
+
+class SplashScreenBody extends StatefulWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody> {
+  _startDelay() {
+    Timer(
+        const Duration(seconds: 5),
+        _goNext
+    );
+  }
+
+  _goNext() {
+    Navigator.of(context).pushReplacementNamed(Routes.welcomeScreen);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Center(
+          child: Image.asset(
+            'assets/images/splash_screen_image.png',
+            fit: BoxFit.cover,
+          ),
+        )
+    );
+  }
+}
